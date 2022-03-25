79e8abb09b03bd648116dd2ab743d1743417dce2
Kartik Gupta <kartik.gupta@octopus.com>
2022-03-25 16:59:15 +1000

    Add script to reassign envs for machines

---
+++ DistributeMachinesEvenlyBetweenEnvironments.ps1
@@ -0,0 +1,24 @@
+# Define working variables
+$octopusURL = "https://busybodyspinaltap.testoctopus.app/"
+$octopusAPIKey = ""
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+$spaceName = "Default"
+$environments = @("Environments-1", "Environments-21", "Environments-41", "Environments-2", "Environments-22", "Environments-61")
+
+# Get space
+$space = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/spaces/all" -Headers $header) | Where-Object {$_.Name -eq $spaceName}
+
+# Get machine list
+$machines = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/machines/all" -Headers $header) | Where-Object {$_}
+
+# Loop through list
+foreach ($machine in $machines)
+{
+    $idx = $machines.IndexOf($machine);
+
+    # Update the machine object
+    $machine.EnvironmentIds = @($environments[$idx%$environments.Count])
+
+    # Update the Machine
+    Invoke-RestMethod -Method Put -Uri "$octopusURL/api/$($space.Id)/machines/$($machine.Id)" -Body ($machine | ConvertTo-Json -Depth 10) -Headers $header
+}
